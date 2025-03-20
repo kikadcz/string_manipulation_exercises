d29d86b87c5669efd1fa256352e3f3c613617f83
Krizean Dela Cruz <delacruz.krizean.barro@gmail.com>
2025-03-20 23:39:04 +0800

    Initial commit

---
+++ count_the_characters_inputted.py
@@ -0,0 +1,7 @@
+#Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
+
+name = input("What's your full name? : ")
+
+character_count = len(name)
+
+print(character_count)
