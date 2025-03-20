f8a793cec1fb7f9369ee978428e2ccc971535203
Krizean Dela Cruz <delacruz.krizean.barro@gmail.com>
2025-03-21 00:44:57 +0800

    Initial commit

--- convert_input_name_into_snake_case.py
+++ convert_input_name_into_snake_case.py
@@ -1,2 +1,7 @@
 #Prog10: Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
 
+name = input("What's your full name? (Use incorrect casing): ")
+
+snake_case = "_".join(name.lower().split())
+
+print(snake_case)
