959fc9365c3037a2784d7a5130716c7ef165ab38
Krizean Dela Cruz <delacruz.krizean.barro@gmail.com>
2025-03-20 18:53:21 +0800

    Initial commit

--- format_input_number_into_six_digits.py
+++ format_input_number_into_six_digits.py
@@ -1,2 +1,3 @@
 #Prog02: Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
 
+number = input(int("Enter a number from 1-1000 :"))
