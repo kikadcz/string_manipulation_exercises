c4f5271cd1520d57d7e45301a8b5f35fd156f5d0
Krizean Dela Cruz <delacruz.krizean.barro@gmail.com>
2025-03-21 00:39:49 +0800

    Initial commit

--- convert_input_into_pascal_case.py
+++ convert_input_into_pascal_case.py
@@ -1,2 +1,8 @@
 #Prog09: Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
 
+name = input("What's your full name? : ")
+
+pascal_name = "".join(name.title().split())
+
+print(pascal_name)
+
