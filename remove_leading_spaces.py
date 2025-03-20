957b7406f25d68f1ce62a45650d33daee3e379cd
Krizean Dela Cruz <delacruz.krizean.barro@gmail.com>
2025-03-20 09:58:07 +0800

    Initial commit

--- remove_leading_spaces.py
+++ remove_leading_spaces.py
@@ -1,8 +1,8 @@
 #Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
 
-name = input("What's your name? (Include several space character before typing your name)")
+name = input("What's your full name? (Include several space character before typing your name):")
 
 name = name.strip()
 
-print(f"Name:", name)
+print(f"Full Name:", name)
 
